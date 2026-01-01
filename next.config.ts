b1eb2995f956cec656b9cef0f7dace712c5106bc
Michael <eramehmichael2000@gmail.com>
2026-01-01 13:15:00 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/',  // Add your subfolder here
-  assetPrefix: '/',
   images: {
     unoptimized: true,
   },
