791453a0d879aaf02373274b1e81bb4ac5e72067
Michael <eramehmichael2000@gmail.com>
2026-01-06 09:42:58 +0100

    update

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="/assets/hero-vid.mp4" type="video/mp4" />
+          <source src="/assets/hero.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         {/* Linear gradient overlay */}
